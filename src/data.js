f10fab5be2119e4ab841528f5b3d3d963cae9959
smekuria1 <smekuria@matignon.org>
2021-12-09 09:05:34 -0500

    Final commit, fixed project image

--- src/data.js
+++ src/data.js
@@ -4,27 +4,10 @@ export const projects = [
     subtitle: "Discord.js Bot",
     description:
       "Discord bot for my personal server that plays soundcloud music using a plugin \n -More features \n Create text Channel,Simple webscraping usig axios and cheerios",
-    image: "./project-1.gif",
+    image: "Solobot.png",
     link: "https://github.com/smekuria1/First_Solo_Bot",
   },
 ];
-
-// export const testimonials = [
-//   {
-//     quote:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-//     image: "https://randomuser.me/api/portraits/men/1.jpg",
-//     name: "Jesse Hicks",
-//     company: "Zoozle",
-//   },
-//   {
-//     quote:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-//     image: "https://randomuser.me/api/portraits/men/94.jpg",
-//     name: "Ruben Alvarez",
-//     company: "Dooble.io",
-//   },
-// ];
 
 export const skills = [
   "JavaScript",
