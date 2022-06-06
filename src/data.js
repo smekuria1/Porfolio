7527962df22aaf169bfce64a60b5b45941050fb9
smekuria1 <smekuria@matignon.org>
2022-06-06 19:56:48 -0400

    Added new Project

--- src/data.js
+++ src/data.js
@@ -1,19 +1,27 @@
 export const projects = [
   {
-    title: "SoloBot",
-    subtitle: "Discord.js Bot",
+    title: 'SoloBot',
+    subtitle: 'Discord.js Bot',
     description:
-      "Discord bot for my personal server that plays soundcloud music using a plugin \n -More features \n Create text Channel,Simple webscraping usig axios and cheerios",
-    image: "Solobot.png",
-    link: "https://github.com/smekuria1/First_Solo_Bot",
+      'Discord bot for my personal server that plays soundcloud music using a plugin \n -More features \n Create text Channel,Simple webscraping usig axios and cheerios',
+    image: 'Solobot.png',
+    link: 'https://github.com/smekuria1/First_Solo_Bot',
+  },
+  {
+    title: 'Reactive Portfolio(React Sanity Node)',
+    subtitle: 'My new Portfolio',
+    description:
+      'After making this website, I wanted to make a new and improved portfolio \n -More features \n Uses a Sanity backend as a CMS \n Uses motion-framer for animations \n Uses SASS instead of CSS for styling',
+    image: 'portfolio.png',
+    link: 'https://github.com/smekuria1/Reactive_portfolio_website',
   },
 ];
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Java/OOP",
-  "Node",
-  "C",
-  "HTML/CSS",
+  'JavaScript',
+  'React',
+  'Java/OOP',
+  'Node',
+  'C',
+  'HTML/CSS',
 ];
